9b74b1e5510c908a1c62674e72404b4b7776ad82
sochetanie <allagorik2901@gmail.com>
2017-05-15 13:15:03 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/' do
+	  "Hello, World!"
+	end
+
+	get '/name' do 
+		"My name is Alla"
+	end
+
+	get '/hometown' do
+		"My hometown is Khabarovsk"
+	end
+
+	get '/favorite-song' do 
+		"My favorite song is Crying in the Rain"
+	end
+
 end
